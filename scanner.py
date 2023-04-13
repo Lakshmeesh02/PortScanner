c107caee83c6ab44a5976dab88f4d509cab054c5
Lakshmeesh Bhat <sherlywatson44@gmail.com>
2023-04-13 22:36:52 +0530

    timeout increased for better scanning

--- scanner.py
+++ scanner.py
@@ -8,15 +8,17 @@ header=pyfiglet.figlet_format("PORT SCANNER")
 print(header)
 
 print('-'*50)
-target_host=input("Enter hostname: ")
-target_ip=socket.gethostbyname(target_host)
+#target_host=input("Enter hostname: ")
+#target_ip=socket.gethostbyname(target_host)
+target_ip=input("Enter ip address: ")
+#print(socket.gethostbyaddr(target_ip)[0])
 print(str(datetime.now()))
 print('-'*50)
 
 try:
-    for port in range(1,1001):
+    for port in range(1,501):
         s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        socket.setdefaulttimeout(0.02)
+        socket.setdefaulttimeout(0.2)
         openport=s.connect_ex((target_ip,port))
         if openport==0:
             print(f"{port} is open..")
