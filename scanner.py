0153503854469ceea257d3b1e7dd47ed6da54550
Lakshmeesh Bhat <sherlywatson44@gmail.com>
2023-04-14 23:09:46 +0530

    displays services running on open ports

--- scanner.py
+++ scanner.py
@@ -8,20 +8,24 @@ header=pyfiglet.figlet_format("PORT SCANNER")
 print(header)
 
 print('-'*50)
-target_host=input("Enter hostname: ")
-target_ip=socket.gethostbyname(target_host)
+target_host=input("Hostname: ")
+try:
+    target_ip=socket.gethostbyname(target_host)
+except:
+    print("Invalid hostname..")
+    sys.exit()
 #target_ip=input("Enter ip address: ")
-#print(socket.gethostbyaddr(target_ip)[0])
-print(str(datetime.now()))
+print(str(datetime.now())[:19])
 print('-'*50)
-
+print("PORT\tSERVICE\n")
+services={22:"ssh", 25:"smtp", 80:"http", 135:"microsoft-rpc", 139:"netbios-ssn", 443:"https", 445:"microsoft-smb", 21:"ftp", 53:"dns"}
 try:
     for port in range(1,501):
         s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         socket.setdefaulttimeout(0.2)
         openport=s.connect_ex((target_ip,port))
         if openport==0:
-            print(f"{port} is open..")
+            print(f"{port}\t{services[port]}")
         s.close()
 
 except KeyboardInterrupt:
@@ -29,3 +33,4 @@ except KeyboardInterrupt:
 except socket.error as e:
     print("Socket error :(",e)
     sys.exit()
+
