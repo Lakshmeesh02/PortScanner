ea14b1608bb2f1826297dd111682fba3eb93164a
Lakshmeesh Bhat <sherlywatson44@gmail.com>
2023-04-14 23:26:10 +0530

    comments for better understanding

--- scanner.py
+++ scanner.py
@@ -3,14 +3,13 @@ import pyfiglet
 import sys
 from datetime import datetime
 
-protocols=[]
-header=pyfiglet.figlet_format("PORT SCANNER")
+header=pyfiglet.figlet_format("PORT SCANNER")   # design banner
 print(header)
 
 print('-'*50)
-target_host=input("Hostname: ")
+target_host=input("Hostname: ")     # get target website name from the user
 try:
-    target_ip=socket.gethostbyname(target_host)
+    target_ip=socket.gethostbyname(target_host)     # try to get the ip address of it (ip addresses are scanned, not the hostnames)
 except:
     print("Invalid hostname..")
     sys.exit()
@@ -18,17 +17,17 @@ except:
 print(str(datetime.now())[:19])
 print('-'*50)
 print("PORT\tSERVICE\n")
-services={22:"ssh", 25:"smtp", 80:"http", 135:"microsoft-rpc", 139:"netbios-ssn", 443:"https", 445:"microsoft-smb", 21:"ftp", 53:"dns"}
+services={22:"ssh", 25:"smtp", 80:"http", 135:"microsoft-rpc", 139:"netbios-ssn", 443:"https", 445:"microsoft-smb", 21:"ftp", 53:"dns"}     # map services for likely-to-be-open ports
 try:
-    for port in range(1,501):
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        socket.setdefaulttimeout(0.2)
-        openport=s.connect_ex((target_ip,port))
+    for port in range(1,501):   # range of ports to scan
+        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)     # create a new socket to connect to each port
+        socket.setdefaulttimeout(0.2)   # time to send a request and recieve a response
+        openport=s.connect_ex((target_ip,port))     # connects to the port in the target (returns 0 if successful, 1 if not)
         if openport==0:
             print(f"{port}\t{services[port]}")
         s.close()
 
-except KeyboardInterrupt:
+except KeyboardInterrupt:       # interrupts
     sys.exit()
 except socket.error as e:
     print("Socket error :(",e)
