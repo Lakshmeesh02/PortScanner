971f50a964b943c7924c593a72afecf1bae3b6a0
Lakshmeesh Bhat <sherlywatson44@gmail.com>
2023-04-19 21:54:09 +0530

    creates a file for every host and documents results of the scan

--- scanner.py
+++ scanner.py
@@ -2,6 +2,7 @@ import socket
 import pyfiglet
 import sys
 from datetime import datetime
+import os
 
 header=pyfiglet.figlet_format("PORT SCANNER")   # design banner
 print(header)
@@ -15,17 +16,24 @@ except:
     sys.exit()
 #target_ip=input("Enter ip address: ")
 print(str(datetime.now())[:19])
+date_time=str(datetime.now())[:19]
 print('-'*50)
 print("PORT\tSERVICE\n")
-services={22:"ssh", 25:"smtp", 80:"http", 135:"microsoft-rpc", 139:"netbios-ssn", 443:"https", 445:"microsoft-smb", 21:"ftp", 53:"dns"}     # map services for likely-to-be-open ports
+services={22:"ssh", 25:"smtp", 80:"http", 135:"microsoft-rpc", 139:"netbios-ssn", 443:"https", 445:"microsoft-smb", 21:"ftp", 53:"dns"}     # map services for likely-to-be-open ports 
 try:
-    for port in range(1,501):   # range of ports to scan
-        s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)     # create a new socket to connect to each port
-        socket.setdefaulttimeout(0.2)   # time to send a request and recieve a response
-        openport=s.connect_ex((target_ip,port))     # connects to the port in the target (returns 0 if successful, 1 if not)
-        if openport==0:
-            print(f"{port}\t{services[port]}")
-        s.close()
+    with open(f"results {target_host}.txt", "w") as f:
+        f.write(date_time)
+        f.write('\n')
+        f.write(f"Target hostname: {target_host}\n")
+        f.write(f"Target IP: {target_ip}\n\n")
+        for port in range(1,501):   # range of ports to scan
+            s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)     # create a new socket to connect to each port
+            socket.setdefaulttimeout(0.2)   # time to send a request and recieve a response
+            openport=s.connect_ex((target_ip,port))     # connects to the port in the target (returns 0 if successful, 1 if not)   
+            if openport==0:
+                print(f"{port}\t{services[port]}")
+                f.write(f"{port}\t{services[port]}\n")
+            s.close()
 
 except KeyboardInterrupt:       # interrupts
     sys.exit()
